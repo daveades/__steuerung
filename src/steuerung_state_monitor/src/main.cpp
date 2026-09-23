5610bed9beeae6c9e9c5ec612558ea2a09dca9d5
dave <jesugbohunmi23@gmail.com>
2026-09-23 17:21:12 +0100

    Add /joinstate subsription node to state_monitor package

--- src/steuerung_state_monitor/src/main.cpp
+++ src/steuerung_state_monitor/src/main.cpp
@@ -1,15 +1,66 @@
-#include <iostream>
+#include <functional>
+#include <memory>
+#include <cstddef>
+#include <sstream>
+
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
 
 
 class StateMonitor: public rclcpp::Node
 {
     public:
-        StateMonitor():
-            Node("stateMonitor")
+    StateMonitor():
+        Node("state_monitor")
+    {
+        subscription_ = this->create_subscription<sensor_msgs::msg::JointState>(
+            "/joint_states", 10, std::bind(&StateMonitor::logJointStates, this, std::placeholders::_1)
+        );
+    }
+    
+    private:
+    void logJointStates(const sensor_msgs::msg::JointState &jointPayload) const
+    {
+        if (jointPayload.name.empty() || jointPayload.position.empty())
         {
-            std::cout << "This is my first Node" << std::endl;
+            RCLCPP_ERROR(
+                this->get_logger(),
+                "Joint Payload is empty: received %zu joint names and %zu positions.",
+                jointPayload.name.size(),
+                jointPayload.position.size()
+            );
+            return;
         }
+        else if (jointPayload.name.size() != jointPayload.position.size())
+        {
+            RCLCPP_ERROR(
+                this->get_logger(),
+                "Joint payload size mismatch: received %zu joint name(s) but %zu position(s).",
+                jointPayload.name.size(),
+                jointPayload.position.size()
+            );
+            return;
+        }
+
+        std::ostringstream strOut;
+
+        for (std::size_t i {0}; i < jointPayload.position.size(); i++)
+        {
+
+            strOut << "\""<< jointPayload.name[i] << "\"" << ": " << jointPayload.position[i];
+
+            if (i != jointPayload.name.size() - 1)
+                strOut << ", ";
+        }
+
+        RCLCPP_INFO(
+            this->get_logger(),
+            "Joint states: {%s}",
+            strOut.str().c_str()
+        );
+
+    }
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr subscription_;
 };
 
 int main(int argc, char **argv)
